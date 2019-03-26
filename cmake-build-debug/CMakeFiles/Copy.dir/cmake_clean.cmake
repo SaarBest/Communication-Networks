2ec3e8ac4849d4373ef18e628e22a8c9c4c92565
Saar <saar157@gmail.com>
2019-03-26 12:08:15 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Copy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Copy.dir/main.c.obj"
+  "Copy.pdb"
+  "Copy.exe"
+  "Copy.exe.manifest"
+  "libCopy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Copy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
